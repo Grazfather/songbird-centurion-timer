8a904ce168a89e4937336947727ed5ad7882e74d
grazfather <grazfather@1a7172ea-e4c1-11dd-9d8c-a70aa0a93a20>
2009-01-17 19:33:01 +0000

    Got buttons working as expected (calling correct function, blanking out buttons appropriately).

--- chrome/content/pane.js
+++ chrome/content/pane.js
@@ -10,8 +10,11 @@ function Game()
 	this.duration;
 	this.frequency;
 	this.playlist;
-	this.started = 0;
+	this.started = false;
 	this.consumed = 0;
+	this.paused = false;
+	var gMM = Components.classes["@songbirdnest.com/Songbird/Mediacore/Manager;1"]
+						.getService(Components.interfaces.sbIMediacoreManager); 
 }
 //methods for our Student class
 Game.prototype.setup = function(players, duration, frequency, playlist)
@@ -19,18 +22,34 @@ Game.prototype.setup = function(players, duration, frequency, playlist)
 	this.players = players;
 	this.duration = duration;
 	this.frequency = frequency;
-	this.playlist = playlist;
+	if (!playlist)
+	{
+		this.playlist = 0;
+	}
+	else
+	{
+		this.playlist = playlist;
+	}
 }
 
 Game.prototype.start = function()
 {
-	//this.started = 1;
-	alert("strd");
+	this.started = true;
+	alert(this.players+" "+this.duration+" "+this.frequency+" "+this.playlist);
 }
+
 Game.prototype.pause = function()
 {
+	this.paused = true;
 	alert("pause");
 }
+
+Game.prototype.resume = function()
+{
+	this.paused = false;
+	alert("resume");
+}
+
 Game.prototype.stop = function()
 {
 	alert("stop");
@@ -42,7 +61,12 @@ Game.prototype.extend = function()
 
 Game.prototype.inProgress = function()
 {
-	return (this.start != 0);
+	return (this.started != 0);
+}
+
+Game.prototype.isPaused = function()
+{
+	return (this.paused != 0);
 }
 
 
@@ -79,21 +103,32 @@ CenturionTimer.PaneController = {
 	
 	this._game1 = document.getElementById("game1");
     this._game1.addEventListener("command", 
-         function() { controller.setGame(100); }, false);
+         function() { controller.setGame(1); }, false);
 	this._game2 = document.getElementById("game2");
     this._game2.addEventListener("command", 
-         function() { controller.setGame(60); }, false);
+         function() { controller.setGame(2); }, false);
 	this._game3 = document.getElementById("game3");
     this._game3.addEventListener("command", 
-         function() { controller.setGame(30); }, false);
-	this._game4 = document.getElementById("game3");
+         function() { controller.setGame(3); }, false);
+	this._game4 = document.getElementById("game4");
     this._game4.addEventListener("command", 
-         function() { controller.setGame(30); }, false);
+         function() { controller.setGame(4); }, false);
 	this._customGame = document.getElementById("customGame");
     this._customGame.addEventListener("command", 
          function() { controller.setGame(0); }, false);
 		 
-	this.defaultGames = [[100, 60],[60,60],[30,60],[20,30]];
+	this.defaultGames = new Array(4);
+	this.defaultGames[0] = [100, 60];
+	this.defaultGames[1] = [60, 60];
+	this.defaultGames[2] = [30, 60];
+	this.defaultGames[3] = [20, 30];
+	
+	//Load up playlists
+	var pls = document.getElementById("playlist-select");
+	var pl = document.createElement("listitem");
+	pl.setAttribute("value", 1);
+	pl.setAttribute("label", "PL1");
+	pls.appendChild(pl);
   },
   
   /**
@@ -107,14 +142,21 @@ CenturionTimer.PaneController = {
    */
    
   startGame: function() {
-	if (this._game.inProgress() != 0)
+	if (this._game.inProgress() == 0 ) // if the game hasn't started yet.
 	{
 		this._game.setup(document.getElementById("players-box").value,
 						 document.getElementById("duration-box").value,
 						 document.getElementById("frequency-box").value,
 						 document.getElementById("playlist-select").value);
 		this._game.start();
-		document.getElementById("start-button").disabled="true";
+		document.getElementById("start-button").setAttribute("disabled","true");
+		document.getElementById("pause-button").setAttribute("disabled","false");
+		document.getElementById("stop-button").setAttribute("disabled","false");
+		document.getElementById("extend-button").setAttribute("disabled","false");
+	}
+	else if (this._game.isPaused() == 1) // Game is started but paused
+	{
+		this._game.resume();
 	}
 	else
 	{
@@ -126,7 +168,16 @@ CenturionTimer.PaneController = {
    * Pause the timer, pause the music
    */
   pauseGame: function() {
-	this._game.pause();
+	if (this._game.isPaused() == 0) // Game is started and running
+	{
+		this._game.pause();
+		document.getElementById("start-button").setAttribute("disabled","false");
+	}
+	else // Game is paused
+	{
+		this._game.resume();
+		document.getElementById("start-button").setAttribute("disabled","true");
+	}
   },
   
   /**
@@ -134,6 +185,10 @@ CenturionTimer.PaneController = {
    */
   stopGame: function() {
 	this._game.stop();
+	document.getElementById("start-button").setAttribute("disabled","false");
+	document.getElementById("pause-button").setAttribute("disabled","true");
+	document.getElementById("stop-button").setAttribute("disabled","true");
+	document.getElementById("extend-button").setAttribute("disabled","true");
   },
   
   /**
@@ -146,8 +201,18 @@ CenturionTimer.PaneController = {
   
   setGame: function(game) {
     if (game != 0) {
-	 document.getElementById("duration-box").value=this.defaultGames[(game-1)][0];
-	 document.getElementById("frequency-box").value=this.defaultGames[(game-1)][1];
+		document.getElementById("duration-box").disabled="true";
+		document.getElementById("frequency-box").disabled="true";
+		document.getElementById("duration-box").value = this.defaultGames[(game-1)][0];
+		document.getElementById("frequency-box").value = this.defaultGames[(game-1)][1];
+		//document.getElementById("duration-box").setAttribute("value",this.defaultGames[(game-1)][0]);
+		//document.getElementById("frequency-box").setAttribute("value",this.defaultGames[(game-1)][1]);
+
+	}
+	else // custom game selected
+	{
+		document.getElementById("duration-box").setAttribute("disabled","false");
+		document.getElementById("frequency-box").setAttribute("disabled","false");
 	}
 
   }
